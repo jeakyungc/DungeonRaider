9d0a4d5a1e0915e2438c69b050065bc399e24c5c
jeakyung <jeakyungc@gmail.com>
2024-03-10 08:43:23 +0900

    Init : "New component for box collider component" Changes to be committed: 	new file:   TriggerComponent.cpp 	new file:   TriggerComponent.h

---
+++ TriggerComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "TriggerComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUNGEONRAIDER_API UTriggerComponent : public UBoxComponent
+{
+	GENERATED_BODY()
+	
+};
